6385b02a03e13fe93e6909d7be9b46b8aba1f911
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-06 16:09:22 -0600

    Added extractDate.js

---
+++ 10_3_extractDate.js
@@ -0,0 +1,12 @@
+﻿var extractDate = function(str) {
+	if (!/\d\d?[\/\-]\d\d?[\/\-]\d{4}/.test(str))
+		throw new Error("Bad date in " + str + ".")
+	return new Date(str)
+}
+
+if (1) {
+	console.log(extractDate("1/2/2013"))
+	console.log(extractDate("1-3-2013"))
+	console.log(extractDate("13/2/2013"))
+	console.log(extractDate("1-2/2013"))
+}
